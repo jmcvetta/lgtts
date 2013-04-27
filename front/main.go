e8226f7d29969c7494a76d8e5e41282c0784769c
Jason McVetta <jason.mcvetta@gmail.com>
2013-04-26 22:32:58 -0700

    front end stub

---
+++ front/main.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2013 Jason McVetta.  This is Free Software, released under the
+// terms of the AGPL v3.  See www.gnu.org/licenses/agpl-3.0.html for details.
+// Resist intellectual serfdom - the ownership of ideas is akin to slavery.
+
+
+package main
+
+func main() {
+
+}
